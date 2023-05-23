9a339427207c6ac7d6a990ebf76e96bf1d65a89b
kg0r0 <kg0r0@yahoo.co.jp>
2023-05-23 23:52:13 +0900

    Add OIDC strategy

--- src/strategy.ts
+++ src/strategy.ts
@@ -1,22 +1,114 @@
 import { Strategy as PassportStrategy } from 'passport-strategy';
 import { AuthenticateOptions } from 'passport'
 import { Request } from 'express';
+import { Issuer, Client, TokenSet, generators } from 'openid-client';
 
-export abstract class AbstractStrategy extends PassportStrategy {
+declare module 'express-session' {
+  export interface SessionData {
+    authParams: AuthParams
+    tokenSet: TokenSet
+    isLoggedIn: Boolean
+  }
+}
+
+export interface AuthParams {
+  scope?: string
+  state?: string
+  nonce?: string
+  codeVerifier?: string
+  codeChallengeMethod?: string
+  originalUrl?: string
+}
+
+export interface StrategyOptions {
+  name?: string
+  authParams?: AuthParams
+  url: string
+  client_id: string
+  client_secret: string
+  redirect_uri: string
+}
+
+export class Strategy extends PassportStrategy {
   name: string;
-  constructor(options?: any) {
+  authParams: AuthParams;
+  client?: Client;
+  issuer?: Issuer;
+  url: string;
+  client_id: string;
+  client_secret: string;
+  redirect_uri: string;
+
+  constructor(options: StrategyOptions) {
     super();
     this.name = options.name || 'example';
+    this.authParams = options.authParams || {};
+    this.client_id = options.client_id;
+    this.client_secret = options.client_secret;
+    this.redirect_uri = options.redirect_uri;
+    this.url = options.url;
   }
 
-  authenticate(req: Request, options: AuthenticateOptions): void {
-    if (options.authInfo === false) {
-      const res = req.res
-      res?.send('error');
+  async authenticate(req: Request, options: AuthenticateOptions): Promise<void> {
+    if (!this.issuer) {
+      this.issuer = await Issuer.discover(this.url)
+    }
+    this.client = new this.issuer.Client({
+      client_id: this.client_id,
+      client_secret: this.client_secret,
+      redirect_uris: [this.redirect_uri],
+      response_types: ['code']
+    })
+    if (!req.session) {
+      throw new Error('express-session is not configured')
+    }
+
+    if (req.session.isLoggedIn) {
+      this.pass();
       return
     }
-    console.log('Logged-In');
-    this.pass();
+
+    if (req.query.code && req.session.authParams) {
+      const state = req.session.authParams.state;
+      const codeVerifier = req.session.authParams.codeVerifier;
+      const nonce = req.session.authParams.nonce;
+      const params = this.client.callbackParams(req);
+      const tokenSet = await this.client.callback(
+        this.redirect_uri,
+        params,
+        {
+          state,
+          nonce,
+          code_verifier: codeVerifier
+        }
+      )
+      req.session.tokenSet = tokenSet;
+      req.session.isLoggedIn = true;
+      return req.res?.redirect(req.session.authParams.originalUrl!);
+    }
+    const scope = 'openid'
+    const nonce = generators.nonce();
+    const state = generators.state();
+    const codeVerifier = generators.codeVerifier();
+    const codeChallenge = generators.codeChallenge(codeVerifier);
+    req.session.authParams = {};
+    req.session.authParams.scope = scope;
+    req.session.authParams.nonce = nonce;
+    req.session.authParams.state = state;
+    req.session.authParams.codeVerifier = codeVerifier;
+    req.session.authParams.codeChallengeMethod = 'S256';
+    req.session.authParams.state = state;
+    req.session.authParams.originalUrl = req.originalUrl;
+    this.authParams = req.session.authParams
+    const url = this.client.authorizationUrl({
+      response_type: 'code',
+      scope,
+      state,
+      nonce,
+      code_challenge: codeChallenge,
+      code_challenge_method: 'S256',
+    })
+    return req.res?.redirect(url);
   }
 
   success(user: any, info?: any): void {
@@ -31,7 +123,7 @@ export abstract class AbstractStrategy extends PassportStrategy {
     super.redirect(url, status);
   }
 
-}
+  verify(): void {
 
-export class Strategy extends AbstractStrategy {
+  }
 }
