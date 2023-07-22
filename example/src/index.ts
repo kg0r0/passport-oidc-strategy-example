031b68090ef630bbf54956066134dd305b880f01
56 <kg0r0@yahoo.co.jp>
2023-07-22 22:56:01 +0900

    Add OIDC strategy (#1)

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,28 +1,29 @@
 import express from 'express';
 import passport from 'passport';
-import { Strategy as ExampleStrategy } from '../../lib/';
+import session from 'express-session';
+import crypto from 'crypto';
+import { Strategy as ExampleStrategy } from '../../lib';
 const app: express.Express = express()
 
-passport.use(new ExampleStrategy({}));
+passport.use(new ExampleStrategy({
+  client_id: '<CLIENT_ID>',
+  client_secret: '<CLIENT_SECRET>',
+  url: 'https://accounts.google.com/.well-known/openid-configuration',
+  redirect_uri: 'http://localhost:3000/cb',
+}));
+
+app.use(session({
+  name: 'session',
+  secret: [crypto.randomBytes(32).toString('hex')],
+  resave: true,
+  saveUninitialized: true
+}));
 
 /**
  * routes
  */
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('OK');
-});
-
-app.get('/login',
+app.get('/*',
   passport.authenticate('example', {}),
-  (req: express.Request, res: express.Response) => {
-    res.send('OK');
-  }
-);
-
-app.get('/error',
-  passport.authenticate('example', {
-    authInfo: false
-  }),
   (req: express.Request, res: express.Response) => {
     res.send('OK');
   }
--- lib/strategy.d.ts
+++ lib/strategy.d.ts
@@ -1,13 +1,43 @@
 import { Strategy as PassportStrategy } from 'passport-strategy';
 import { AuthenticateOptions } from 'passport';
 import { Request } from 'express';
-export declare abstract class AbstractStrategy extends PassportStrategy {
+import { Issuer, Client, TokenSet } from 'openid-client';
+declare module 'express-session' {
+    interface SessionData {
+        authParams: AuthParams;
+        tokenSet: TokenSet;
+        isLoggedIn: Boolean;
+    }
+}
+export interface AuthParams {
+    scope?: string;
+    state?: string;
+    nonce?: string;
+    codeVerifier?: string;
+    codeChallengeMethod?: string;
+    originalUrl?: string;
+}
+export interface StrategyOptions {
+    name?: string;
+    authParams?: AuthParams;
+    url: string;
+    client_id: string;
+    client_secret: string;
+    redirect_uri: string;
+}
+export declare class Strategy extends PassportStrategy {
     name: string;
-    constructor(options?: any);
-    authenticate(req: Request, options: AuthenticateOptions): void;
+    authParams: AuthParams;
+    client?: Client;
+    issuer?: Issuer;
+    url: string;
+    client_id: string;
+    client_secret: string;
+    redirect_uri: string;
+    constructor(options: StrategyOptions);
+    authenticate(req: Request, options: AuthenticateOptions): Promise<void>;
     success(user: any, info?: any): void;
     error(err: Error): void;
     redirect(url: string, status?: number): void;
-}
-export declare class Strategy extends AbstractStrategy {
+    verify(): void;
 }
